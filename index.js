5800382b3334d20412f541a049e35cb40549f20e
TJ Hooker <tyrelhooker@gmail.com>
2020-10-18 00:06:20 -0400

    Implement Delete route, single person GET, single person POST, and error handling for missing data or duplicate name

--- index.js
+++ index.js
@@ -6,23 +6,77 @@ const app = express();
 app.use(express.json());
 
 
-const persons = db.persons.map(person => person);
-// const persons = JSON.parse(db);
+let persons = db.persons.map(person => person);
+console.log(persons);
 
-app.get("/api/persons", (request, response) => {
-  response.json(persons);
+const generateRandomId = () => Math.floor(Math.random() * 10000 + 1);
+
+// ROUTES
+app.get('/api/persons', (req, res) => {
+  res.json(persons);
 })
 
-app.get("/info", (request, response) => {
+app.get('/info', (req, res) => {
   const totalPersons = persons.length;
   console.log(totalPersons);
   // console.log(persons.length)
   const calcDate = new Date();
   
-  response.send(
+  res.send(
     `<p>Phonebook has info for ${totalPersons} people.</p>
     <p>${calcDate}</p>`
   )
+})
+
+app.get('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const person = persons.find(person => person.id === id);
+
+  if(person) {
+    res.json(person)
+  } else {
+    res.status(404).end();
+  }
+})
+
+app.delete('/api/persons/:id', (req, res) => {
+  const id = Number(req.params.id);
+  const newPersons = persons.filter(person => person.id !== id);
+  persons = newPersons;
+  console.log(persons);
+
+  res.status(404).send('person deleted').end();
+})
+
+app.post('/api/persons', (req, res) => {
+  const body = req.body;
+
+  const nameCheck = persons.find(person => person.name === body.name);
+
+  if (!body.name ) {
+    return res.status(400).json({error: 'name missing'})
+  }
+
+  if (!body.number) {
+    return res.status(400).json({error: 'number missing'})
+  }
+
+  if(nameCheck) {
+    return res.status(400).json({ error: 'Name already exists'})
+  }
+
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: generateRandomId()
+  }
+
+  persons = persons.concat(person);
+  console.log(persons);
+
+  res.json(person);
+
 })
 
 const PORT = 3001;
