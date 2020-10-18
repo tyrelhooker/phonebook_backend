cc25204e0ab8ab0450d3b5358739985aebb67fb2
TJ Hooker <tyrelhooker@gmail.com>
2020-10-17 22:16:08 -0400

    Create route for retrieving total people in phonebook and date

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
-const persons = require("./db.json");
+const db = require("./db.json");
 
 const app = express();
 
 app.use(express.json());
 
-console.log(persons);
+
+const persons = db.persons.map(person => person);
 // const persons = JSON.parse(db);
 
 app.get("/api/persons", (request, response) => {
   response.json(persons);
+})
+
+app.get("/info", (request, response) => {
+  const totalPersons = persons.length;
+  console.log(totalPersons);
+  // console.log(persons.length)
+  const calcDate = new Date();
+  
+  response.send(
+    `<p>Phonebook has info for ${totalPersons} people.</p>
+    <p>${calcDate}</p>`
+  )
 })
 
 const PORT = 3001;
